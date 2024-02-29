0ec77200ae235a9011c0ad41286701207a0f203c
soto12345 <diegoivansotoarreola@gmail.com>
2024-02-29 17:07:13 -0600

    se agrego modelo usuario, autenticacion, token y desaparecer password del usuario

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -45,7 +45,7 @@ router.get(
     }
   }
 )
-
+//comentario
 router.get(
   '/',
   passport.authenticate('jwt', { session: false }),
@@ -58,5 +58,55 @@ router.get(
     }
   }
 )
+ 
+//consultar todas las categorias
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await service.findAll()
+    res.status(200).json(categories)
+  } catch (error) {
+    next(error)
+  }
+})
+//metodo de buscar categoria mediante el id
+router.get('/id/:id', async (req, res, next) => {
+  try {
+    const category = await service.findById(req.params.id)
+    res.status(200).json(category)
+  } catch (error) {
+    next(error)
+  }
+})
+//metodo de buscar categoria mediante el nombre
+router.get('/name/:name', async (req, res, next) => {
+  try {
+    const category = await service.findByName(req.params.name as string)
+    res.status(200).json(category)
+  } catch (error) {
+    next(error)
+  }
+})
+//metodo de eliminar categoria 
+router.get('/delete/:name', async(req, res, next)=>{
+  try{
+    const category = await service.delete(req.params.name as string)
+    res.status(200).json(category)
+    res.send("categoria eliminada correctamente")
+  }catch(error){
+    next(error)
+  }
+})
+//metodo de actualizar nombre de la categoria
+router.post('/update/', async(req,res,next)=>{
+  try{
+    const category_filter: string=req.body.filter
+    const category_name: string=req.body.name
+    const update_category = await service.update_name(category_filter,category_name);
+    res.status(200).json(update_category)
+    res.send("nombre de la categoria actualizada")
+  }catch(error){
+    next(error)
+  }
+})
 
 export default router
